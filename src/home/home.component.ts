7f84d66c5937b2263ba6985350aae14f4bdc6bd8
Lady-Flo <braunsbongi@gmail.com>
2024-05-19 15:50:29 +0200

    basics progress

--- src/home/home.component.ts
+++ src/home/home.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [MatListModule, MatIconModule, CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
-
+  
+  values = [
+    'innovation',
+    'integrity',
+    'sustainability',
+    'flexibility',
+    'collaboration',
+  ];
 }
