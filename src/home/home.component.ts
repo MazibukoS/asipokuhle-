53531be1ed59c4efd2ad41f7f0a8a38b2ba10053
Lady-Flo <braunsbongi@gmail.com>
2024-05-26 21:56:42 +0200

    home page 1st draft done.

--- src/home/home.component.ts
+++ src/home/home.component.ts
@@ -11,7 +11,6 @@ import { CommonModule } from '@angular/common';
   styleUrl: './home.component.css',
 })
 export class HomeComponent {
-  
   values = [
     'innovation',
     'integrity',
@@ -19,4 +18,13 @@ export class HomeComponent {
     'flexibility',
     'collaboration',
   ];
+
+  toggleVisibility(hideClass: string, showClass: string) {
+    const hideElement = document.querySelector('.' + hideClass) as HTMLElement;
+    const showElement = document.querySelector('.' + showClass) as HTMLElement;
+    if (hideElement && showElement) {
+      hideElement.style.display = 'none';
+      showElement.style.display = 'block';
+    }
+  }
 }
